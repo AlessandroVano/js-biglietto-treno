4b5b05eed3a04d920ac3b8a45fb43cf7c0f9dc94
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-21 13:45:05 +0200

    primo step

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+/**
+ * BIGLIETTI DEL TRENO
+ * 
+ * 1. richiesta del numero di chilometri che si vogliono percorrere 
+ * 2. richesta età del passeggero
+ * 3. calcolo del prezzo per n° chilomentri (o,21 euro a km)
+ * 4. applicazione sconto per minorenni (20%)
+ * 5. applicazione sconto per over 65 (40%)
+ */
 
-console.log('hello js')
+
