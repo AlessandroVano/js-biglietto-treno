bcd74d4ed7ec1587e2fadecf4371f5abbe983a0a
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-21 17:51:12 +0200

    calcolo del prezzo x n° km + calcolo biglietto totale

--- js/main.js
+++ js/main.js
@@ -4,17 +4,25 @@
  * 1. richiesta del numero di chilometri che si vogliono percorrere 
  * 2. richesta età del passeggero
  * 3. calcolo del prezzo per n° chilomentri (o,21 euro a km)
- * 4. applicazione sconto per minorenni (20%)
- * 5. applicazione sconto per over 65 (40%)
+ * 4. applicazione sconto 
  */
 
 
 // 1. richiesta del numero di chilometri che si vogliono percorrere 
-const km = prompt('quanti chilometri vuoi percorrere?');
-console.log ('numero chilometri' , km);
+const km = parseInt( prompt('quanti chilometri vuoi percorrere?') );
+console.log ('numero chilometri' ,  km);
 
 // 2. richiesta età del passeggero
-const age = prompt('quanti anni hai?');
-console.log ('numero di anni' , age);
+const age = parseInt( prompt('quanti anni hai?') );
+console.log ('numero di anni' ,  age);
+
+// 3. calcolo del prezzo per n° chilomentri
+
+const prezzo = km * 0.21;
+console.log('prezzo del biglietto in base a km' , prezzo);
+
+/* calcolo del prezzo totale del biglietto */
+let prezzofinale = prezzo * km;
+console.log('prezzo finale del biglietto' , prezzofinale);
 
 
